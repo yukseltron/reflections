54b5c43e5e7c5aa2fec73106151ff7d44671359b
yukseltron <h.yuksel04@gmail.com>
2022-09-20 15:56:10 -0400

    playing with images

---
+++ equalAngles.js
@@ -0,0 +1,82 @@
+function draw() {
+  cnv = createCanvas(windowWidth/2, windowHeight/2);
+  background(0);
+  translate(100,100)
+
+  let v0 = createVector(0,0);
+  let v1 = createVector(mouseX, mouseY);
+  drawArrow(v0, v1, 'orange');
+  fill('green');
+  rect(0,0,100,20);
+
+
+  let n = createVector(0, 1);
+  drawMirror(v1, n, -40, 5, 100, 1);
+
+  let angleBetween = v1.angleBetween(n);
+  let rad = radians(-(90-degrees(angleBetween)));
+  let v2 = p5.Vector.fromAngle(rad, v1.mag());
+  drawArrow(v1, v2, 'red');
+
+  fill(250);
+  text(
+      (90-degrees(angleBetween)).toFixed(2) +
+      '°',
+      mouseX - 75,
+      mouseY - 15,
+      90,
+      50
+  );
+  text(
+      (degrees(angleBetween)).toFixed(2) +
+      '°',
+      mouseX - 45,
+      mouseY - 55,
+      90,
+      50
+  );
+  text(
+      (90-degrees(angleBetween)).toFixed(2) +
+      '°',
+      mouseX + 45,
+      mouseY - 15,
+      90,
+      50
+  );
+  text(
+      (degrees(angleBetween)).toFixed(2) +
+      '°',
+      mouseX + 15,
+      mouseY - 55,
+      90,
+      50
+  );
+}
+
+// draw an arrow for a vector at a given base position
+function drawArrow(base, vec, myColor) {
+  push();
+  stroke(myColor);
+  fill(myColor);
+  translate(base.x, base.y);
+  line(0, 0, vec.x, vec.y);
+  rotate(vec.heading());
+  let arrowSize = 7;
+  translate(vec.mag() - arrowSize, 0);
+  triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+  pop();
+}
+
+function drawMirror(base, vec, x, y, w, h) {
+    push();
+    stroke('#FFFFFF');
+    strokeWeight(1);
+    translate(base.x, base.y);
+    line(0, -100, 0, 0);
+    strokeWeight(10);
+    let arrowSize = 7;
+    stroke('#0000FF');
+    translate(vec.mag() - arrowSize, 0);
+    rect(x, y, w, h);
+    pop();
+}
--- reflect4.js
+++ reflect4.js
@@ -11,11 +11,10 @@ function draw() {
   const x2 = 20;
   const y2 = 0;
 
-  let light = drawLine(mouseX-400, mouseY-400,0,0,'#ffffff');
-  let m1 = drawMirror(x1,y1,x2,y2,light,'red');
-  let {base, vec} = m1;
-  let l2 = drawLine(base.x,base.y,vec.x,vec.y,'#ffffff');
-  let m2 = drawMirror(40, -50, 100, -50,l2,'red');
+  let light = drawLine(mouseX-400, mouseY-400,0,0,'yellow');
+  fill('red');
+  circle(mouseX-400, mouseY-400, 20);
+  let m1 = drawMirror(x1,y1,x2,y2,light);
 
 }
 
@@ -34,22 +33,26 @@ function drawArrow(base, vec, color) {
     pop();
 }
 
-function drawMirror(x1, y1, x2, y2, {base, vec}, color) {
+function drawMirror(x1, y1, x2, y2, {base, vec}) {
     let v1 = createVector(x1,y1);
     let v2 = createVector(x2,y2);
+    drawArrow(v1, v2, 'lightblue');
 
     let dx = x2 - x1;
     let dy = y2 - y1;
-
     let n1 = createVector(-dy,dx);
     let n2 = createVector(dy,-dx);
+    drawArrow(n1, n2, 'white');
+
     let angle = base.angleBetween(v1);
     let v = p5.Vector.fromAngle(angle, base.mag());
-    //print(degrees(angle));
-
-    drawArrow(v1, v2, color);
-    drawArrow(n1, n2, 'green');
     drawArrow(vec, v, 'orange');
+
+    let image = base.copy();
+    image.reflect(n1);
+    drawArrow(vec, image, 'violet');
+    fill('pink');
+    circle(image.x, image.y, 20);
 
     return {base: vec, vec: v};
 }
